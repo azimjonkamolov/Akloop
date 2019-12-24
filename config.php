786a886ce118549abd6d9fc9328c62f648db8333
Azimjon Kamolov <azimjon.6561@gmail.com>
2019-12-24 22:41:57 +0900

    Database fix

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     ob_start();
     try
     {
-	$con = new PDO("mysql:dbname=akloopus; host=localhost", "root", "");
+	$con = new PDO("mysql:dbname=akloop; host=localhost", "root", "");
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
     catch(PODExeption $e)
