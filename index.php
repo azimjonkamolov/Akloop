896255d7f0d130b09031f8c9e7b79987acf5f4c1
Azimjon Kamolov <azimjon.6561@gmail.com>
2020-02-09 21:58:03 +0900

    cleaned the files

--- index.php
+++ index.php
@@ -18,11 +18,9 @@
 
       <li class="dropdown">
           <a href="javascript:void(0)" class="dropbtn">
-            
               <div class="logo"></div>
               <div class="logo"></div>
               <div class="logo"></div>
-
           </a>
           <div class="dropdown-content">
             <a href="#">Text Qidir</a>
@@ -32,17 +30,8 @@
         </li>
 
     </div>
-
-
-
     <section class="index__header">
       <div class="index__logo"></div>
-      <!-- <ul class="index__nav">
-        <li class="index__active"><a></a></li>
-        <li><a href="serp.htm"></a></li>
-        <li><a href="serp.htm"></a></li>
-        <li><a href="serp.htm"></a></li>
-      </ul> -->
       <div class="index__search">
         <form class="index__form" action="search.php" methode="GET">
           <div>
@@ -69,29 +58,9 @@
           <li><a href="#about">Technologies</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#about">Privacy</a></li>
-          <!-- <li><a href="#about">Terms</a></li>
-          <li><a href="#about">Settings</a></li> -->
         </ul>
-        <!-- <p class="index__copyright">&copy; Azimjon Kamolov</p> -->
       </div>
     </footer>
   </div>
-  <!-- Modal -->
-  <!-- <div class="modal" id="about" aria-hidden="true">
-    <div class="modal__dialog">
-      <div class="modal__header">
-        <h2>About</h2>
-        <a href="#" class="modal__close" aria-hidden="true">×</a>
-      </div>
-      <div class="modal__body">
-        <strong>Lorem Ipsum</strong><br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-      <div class="modal__footer">
-        <p>Last Update 01.01.2019</p>
-      </div>
-    </div>
-  </div> -->
-  <!-- /Modal -->
 </body>
 </html>
